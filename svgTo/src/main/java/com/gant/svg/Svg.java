f83be25f3a1998b2497bf8bed663fbfdb20d5c3a
jyp <jyp10@qq.com>
2017-02-10 17:30:36 +0800

    Visual Paradigm 导出的 .svg 文件去水印后转成  .png 文件

---
+++ svgTo/src/main/java/com/gant/svg/Svg.java
@@ -0,0 +1,77 @@
+package com.gant.svg;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Svg {
+
+	private static final String NODE_VALUE = "Visual Paradigm [evaluation copy] ";
+
+	public Document createSvgDoc(String uri) {
+		String parser = XMLResourceDescriptor.getXMLParserClassName();
+		SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
+		Document doc = null;
+		try {
+			doc = f.createDocument(uri);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return doc;
+	}
+
+	public NodeList getNodes(Document doc) {
+		// 根节点 <svg></svg>
+		Element element = doc.getDocumentElement();
+		NodeList nodes = element.getChildNodes();
+		return nodes;
+	}
+
+	public void removeNodes(NodeList nodes) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node.hasChildNodes()) {
+				removeNodes(node.getChildNodes());
+			}
+			if (NODE_VALUE.equals(node.getNodeValue())) {
+				node.getParentNode().removeChild(node);
+			}
+		}
+	}
+
+	public void readNodes(NodeList nodes) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node.hasChildNodes()) {
+				readNodes(node.getChildNodes());
+			}
+
+			String nodeName = node.getNodeName();
+			String nodeValue = node.getNodeValue();
+
+			System.out.println(i + " name:" + nodeName + " value:" + nodeValue);
+			System.out.println();
+
+		}
+	}
+
+	public void printSvgFile(String path, Document document) throws IOException {
+		File file = new File(path);
+		
+		
+		SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+		
+		Writer out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+		svgGenerator.stream(out, true);;
+	}
+}
---
+++ svgTo/src/main/java/com/gant/svg/SvgUtils.java
@@ -0,0 +1,77 @@
+package com.gant.svg;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.batik.transcoder.Transcoder;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class SvgUtils {
+
+	public static Svg svg = new Svg();
+
+	public static void convert2Png(File svgFile, File jpgFile) throws IOException, TranscoderException {
+		InputStream in = new BufferedInputStream(new FileInputStream(svgFile));
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(jpgFile));
+		convert2Png(in, out);
+	}
+
+	public static void convert2Png(InputStream in, OutputStream out) throws IOException, TranscoderException {
+		Transcoder transcoder = new PNGTranscoder();
+		try {
+			TranscoderInput input = new TranscoderInput(in);
+			try {
+				TranscoderOutput output = new TranscoderOutput(out);
+				transcoder.transcode(input, output);
+				out.flush();
+			} finally {
+				out.close();
+			}
+		} finally {
+			in.close();
+		}
+	}
+
+	public static void convert2Png(Document document, File jpgFile) throws IOException, TranscoderException {
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(jpgFile));
+		convert2Png(document, out);
+	}
+
+	public static void convert2Png(Document document, OutputStream out) throws IOException, TranscoderException {
+		Transcoder transcoder = new PNGTranscoder();
+		TranscoderInput input = new TranscoderInput(document);
+		try {
+			TranscoderOutput output = new TranscoderOutput(out);
+			transcoder.transcode(input, output);
+			out.flush();
+		} finally {
+			out.close();
+		}
+	}
+
+	public static void removeWatermark(String uri, String pngFilePath) {
+		Document doc = svg.createSvgDoc(uri);
+		NodeList nodes = svg.getNodes(doc);
+		svg.removeNodes(nodes);
+		File jpg = new File(pngFilePath);
+		try {
+			jpg.createNewFile();
+			SvgUtils.convert2Png(doc, jpg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TranscoderException e) {
+			e.printStackTrace();
+		}
+	}
+}
